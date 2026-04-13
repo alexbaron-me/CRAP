af4eeaa57b3bad2c71f470540fd27c35cbf95d09
Alexander Baron <hey@alexbaron.me>
2026-04-13 20:30:15 +0200

    feat: basic prompting

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { config } from 'dotenv'
+import { Anthropic } from "@anthropic-ai/sdk";
 
 async function main() {
 	config()
 
-	console.log(process.env.CLAUDE_API_KEY)
+	const client = new Anthropic({
+		baseURL: "https://gateway.ai.itestra.com"
+	});
+
+	const stream = await client.messages.stream({
+		max_tokens: 1024,
+		messages: [{ role: "user", content: "Hello there!" }],
+		model: "devstral-small-2",
+		stream: true
+	}).on("text", (text) => process.stdout.write(text));
+
+	const message = await stream.finalMessage();
+	console.log(`\n${message.usage.input_tokens} input tokens, ${message.usage.output_tokens} output tokens`);
 }
 
 main()
